f13e3f742692654d9beaf9d366ebd2b4cc9dae8c
Polina Karotkya <apollinariyad09@gmail.com>
2026-09-22 20:05:30 +0300

    add validation to fields

--- BookStore.Api/Dtos/CreateBook.cs
+++ BookStore.Api/Dtos/CreateBook.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Api.Dtos;
 
 public record class CreateNewBook(
-   string Name,
-    string Genre,
-    int Pages,
-    decimal Price,
+    [Required] [StringLength(50)] string Name,
+    [Required] [StringLength(20)] string Genre,
+    [Range(1, 2000)]int Pages,
+    [Range(1, 400)]decimal Price,
     DateOnly ReleaseDate
 );
--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -1,9 +1,9 @@
-using BookStore.Api.Dtos;
 using BookStore.Api.Enpoints;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation();
+
 var app = builder.Build();
 
 app.MapBooksEndpoints();
